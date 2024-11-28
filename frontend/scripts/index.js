c0ba6cfd83a45917692bfb553432c24350c62e15
Ruan Marra <ruanm72@gmail.com>
2024-11-27 21:19:37 -0300

    adicionado modal para novo lembrete e js para abrir/fechar o modal

---
+++ frontend/scripts/index.js
@@ -0,0 +1,10 @@
+// selecionar modal
+const modal = document.getElementById("lembretes-modal");
+
+function abrirModal() {
+  modal.style.display = "flex";
+}
+
+function fecharModal() {
+  modal.style.display = "none";
+}
