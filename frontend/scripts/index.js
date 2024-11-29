f2df9d48781baedd4362425cae308ad4b28c75a5
Ruan Marra <ruanm72@gmail.com>
2024-11-29 02:20:56 -0300

    Pagina de login adicionada

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -49,8 +49,8 @@ criarLembreteForm.addEventListener("submit", async function (event) {
 });
 
 async function criarLembrete() {
-  const title = document.getElementById("title").value;
-  const content = document.getElementById("content").value;
+  const title = document.getElementById("form-title-input").value;
+  const content = document.getElementById("form-content").value;
   try {
     const resposta = await api.post("/lembretes", {
       title,
