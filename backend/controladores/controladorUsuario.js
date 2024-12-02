708f205c111b42655e90d8fb66f9e9165d13d066
Ruan Marra <ruanm72@gmail.com>
2024-12-01 23:25:06 -0300

    add: bcrypt, criptografia de senha

--- backend/controladores/controladorUsuario.js
+++ backend/controladores/controladorUsuario.js
@@ -1,14 +1,18 @@
 // Controladores que criam e fazem login de usuario
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
+const bcrypt = require("bcrypt");
 
 const criarUsuario = async (req, res) => {
   try {
     // controlador que vai ser chamado quando houver uma requisição para /api/auth/criar-usuario
     const { email, password } = req.body;
-    console.log(email, password);
+    if (!email || !password)
+      return res.status(400).json({ res: "Faltando senha/email" });
+    const salt = 10;
+    const passwordCriptografado = await bcrypt.hash(password, salt);
     const novoUsuario = await prisma.user.create({
-      data: { email, password },
+      data: { email, password: passwordCriptografado },
     });
     const { id } = novoUsuario;
     res.status(200).json({ id });
@@ -18,11 +22,14 @@ const criarUsuario = async (req, res) => {
 };
 
 const loginUsuario = async (req, res) => {
+  const { email, password } = req.body;
   try {
-    const { email, password } = req.body;
     console.log(email, password);
+    if (!email || !password)
+      return res.status(400).json({ res: "Faltando senha/email" });
     const user = await prisma.user.findUnique({ where: { email } });
-    if (!user || user.password !== password) {
+    const senhaCorreta = await bcrypt.compare(password, user.password);
+    if (!senhaCorreta) {
       return res.status(401).json({ mensagem: "Email ou Senha errado." });
     }
     const { id } = user;
